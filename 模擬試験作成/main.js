5c2279edc3f3265c7e95bbe28fc42ae2b8561f27
hiroshi0120 <74820797+hiroshi0120@users.noreply.github.com>
2023-02-09 22:28:28 +0900

    Create main.js
    ボタンの追加

---
+++ 模擬試験作成/main.js
@@ -0,0 +1,16 @@
+function mogi1() {
+    let change = document.getElementById('btn1');
+console.log("開始")
+}
+function mogi2() {
+    let change = document.getElementById('btn2');
+console.log("開始")
+}
+function mogi3() {
+    let change = document.getElementById('btn3');
+console.log("開始")
+}
+function mogi4() {
+    let change = document.getElementById('btn4');
+console.log("開始")
+}
