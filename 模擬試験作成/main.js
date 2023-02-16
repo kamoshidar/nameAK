382cdce3d84497a362de3f3ff6939b28e1cab556
kamoshidar <108622375+kamoshidar@users.noreply.github.com>
2023-02-16 20:48:23 +0900

    Update main.js

--- 模擬試験作成/main.js
+++ 模擬試験作成/main.js
@@ -1,42 +1,27 @@
+
 function mogi1() {
-    let change = document.getElementById('btn1');
-    let change1 = document.getElementById('btn2');
-    let change2 = document.getElementById('btn3');
-     change.textContent = '試験時間は120分です。';
-     change1.textContent = '問題は100問です。';
-     change2.textContent = '準備が出来次第開始ボタンを押してください';
     alert('模擬試験1を選択しました');
 console.log("開始")
 }
 function mogi2() {
-    let change = document.getElementById('btn1');
-    let change1 = document.getElementById('btn2');
-    let change2 = document.getElementById('btn3');
-     change.textContent = '試験時間は135分です。';
-     change1.textContent = '問題は110問です。';
-     change2.textContent = '準備が出来次第開始ボタンを押してください';
     alert('模擬試験2を選択しました');
-console.log("開始")
 }
 function mogi3() {
-    let change = document.getElementById('btn1');
-    let change1 = document.getElementById('btn2');
-    let change2 = document.getElementById('btn3');
-     change.textContent = '試験時間は90分です。';
-     change1.textContent = '問題は90問です。';
-     change2.textContent = '準備が出来次第開始ボタンを押してください';
     alert('模擬試験3を選択しました');
 console.log("開始")
 }
 function mogi4() {
-    let change = document.getElementById('btn1');
-    let change1 = document.getElementById('btn2');
-    let change2 = document.getElementById('btn3');
-     change.textContent = '試験時間は80分です。';
-     change1.textContent = '問題は80問です。';
-     change2.textContent = '準備が出来次第開始ボタンを押してください';
     alert('模擬試験4を選択しました');
 console.log("開始")
+}
+
+function confirmFunction1() {
+    //ret変数に確認ダイアログの結果を代入する。
+ret = confirm("開始しても大丈夫でしょうか？");
+//確認ダイアログの結果がOKの場合外部リンクを開く
+if (ret == true){
+    location.href = "./Testpaje/test1.html";
+ }
 }
 
 
