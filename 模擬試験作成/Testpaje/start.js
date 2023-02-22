7ecbc45b3987b45a46996b7f7218b8e2515599d2
kamoshidar <108622375+kamoshidar@users.noreply.github.com>
2023-02-23 00:09:25 +0900

    Update start.js

--- 模擬試験作成/Testpaje/start.js
+++ 模擬試験作成/Testpaje/start.js
@@ -8,9 +8,11 @@ const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
 const submitBtn = document.getElementById('submit')
  
- 
-let currentQuiz = 0
+ let miss = 0
+ let miss1 = []
+ let currentQuiz = 0
 let score = 0
+let i = 0
  
 loadQuiz()
  
@@ -19,7 +21,9 @@ function loadQuiz() {
   deselectAnswers()
  
   const currentQuizData = quizData[currentQuiz]
- 
+  
+  toi.innerHTML = i + 1 + "問目"
+
   questionEl.innerText = currentQuizData.question
   a_text.innerText = currentQuizData.a
   b_text.innerText = currentQuizData.b
@@ -50,26 +54,51 @@ function getSelected() {
 submitBtn.addEventListener('click', () => {
   // 回答取得
   const answer = getSelected()
+  let gouhi = ""
  
   // 未選択の場合は送信できないようにする
   if(answer) {
       // 正誤チェック
       if(answer === quizData[currentQuiz].correct) {
-          // 正解の場合はスコアをインクリメント
+          miss1[i] = 100
           score++
+      }else {
+           miss++
+           miss1[i] = currentQuiz
       }
  
       currentQuiz++
+      i++
  
       if(currentQuiz < quizData.length) {
           // クイズの読み込み
           loadQuiz()
       } else {
-          // 結果発表
+        if(score >= 8){
+          gouhi = '合格'
+        } else{
+          gouhi = '不合格'
+        }
+
           quiz.innerHTML = `
               <h2>正解数 ${quizData.length}問中${score}問です </h2>
-              <button onclick="location.reload()">Reload</button>
+              <h2>あなたは${gouhi}です。</h2>
+              <h2>不正解は${miss}問です。</h2>
+              <button id="button1" onclick="matigai()">不正解問表示</button>
           `
       }
   }
 })
+
+function matigai(){
+  document.write("------------------------------------------------")
+  for(let l = 0; l < miss1.length; l++){
+    if(miss1[l] != 100){
+      document.write("<p><h3>" + quizData[l].question + "</h3></p>")
+      document.write("<p>答えは" + quizData[l].correct + "です</p>")
+      document.write("<p>" + quizData[l].kaisetu + "</p>")
+      document.write("------------------------------------------------")
+    }
+  }
+  document.write("お疲れ様でした。")
+}
